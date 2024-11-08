3f6c439eb0a3156884fedd99a80c5509b5e35c49
PriyankaCheera <priyankacheera17@gmail.com>
2024-11-08 18:16:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+    function toggleMenu() {
+        const navList = document.getElementById("nav-list");
+        navList.style.display = navList.style.display === "flex" ? "none" : "flex";
+    }
+
+var tablinks=document.getElementsByClassName('tab-links');
+var tabcontents=document.getElementsByClassName('tab-contents');
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove('active-link');
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove('active-tab')
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabname).classList.add('active-tab');
+}
